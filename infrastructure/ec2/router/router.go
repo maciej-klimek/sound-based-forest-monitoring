8994624902d530101aa35196e355ddbab3ed332a
maciej-klimek <maciej.klimek09@gmail.com>
2025-11-25 21:16:17 +0100

    Initial preparation for front-backend communication

--- infrastructure/ec2/router/router.go
+++ infrastructure/ec2/router/router.go
@@ -1,12 +1,16 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/maciej-klimek/sound-based-forest-monitoring/infrastructure/ec2/handlers"
 )
 
 func SetupRouter(handler *handlers.Handler) *gin.Engine {
 	r := gin.Default()
+
+	// Setup CORS middleware
+	r.Use(cors.Default())
 
 	sensors := r.Group("/sensors")
 	{
